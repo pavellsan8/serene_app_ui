9d85de0f2ecbaa149375141ed6b57d037cd44c8c
pavellsan8 <pavelryan146@gmail.com>
2025-03-14 20:55:22 +0700

    Adding logout feature

---
+++ lib/viewmodels/auth/logout_viewmodel.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../services/auth/logout_service.dart';
+import '../../utils/routes.dart';
+
+class LogoutViewModel extends ChangeNotifier {
+  final LogoutService _logoutService = LogoutService();
+  bool isLoading = false;
+
+  Future<void> logout(BuildContext context) async {
+    if (isLoading) return;
+
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      final response = await _logoutService.logoutUser();
+      if (!context.mounted) return;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(response.message)),
+      );
+
+      if (response.status == 200) {
+        Navigator.pushNamed(
+          context,
+          AppRoutes.login,
+          // arguments: this,
+        );
+      }
+    } catch (e) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Logout Failed: ${e.toString()}")),
+      );
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
