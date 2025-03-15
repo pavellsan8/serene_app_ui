c3fc42e9a37f2e5bf8aadb507bf95fa852c73ff2
pavellsan8 <pavelryan146@gmail.com>
2025-03-15 10:22:27 +0700

    Merge code from feature/home

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -26,6 +26,7 @@ class AppProviders {
         ChangeNotifierProvider(create: (_) => FeelingViewModel()),
         ChangeNotifierProvider(create: (_) => MoodViewModel()),
         ChangeNotifierProvider(create: (_) => EmotionViewModel()),
+        
         ChangeNotifierProvider(create: (_) => UserProfileViewModel()),
         ChangeNotifierProvider(create: (_) => LogoutViewModel()),
       ],
