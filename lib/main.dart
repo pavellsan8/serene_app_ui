25aa45f6deb8caead07a66d98288cf4e90d0f266
pavellsan8 <pavelryan146@gmail.com>
2025-05-31 14:08:40 +0700

    Change to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class SereneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.emailInput,
+      initialRoute: AppRoutes.splashScreen,
       routes: AppRoutes.getRoutes(),
       theme: AppColors.lightTheme,
       darkTheme: AppColors.darkTheme,
